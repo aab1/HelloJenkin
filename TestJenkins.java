14ac3b3cee66156f4aa8618a4bf7d672a4f79873
aab1 <yemibakare@yahoo.com>
2017-04-11 21:38:49 +0100

    The java program in jenkin workspace has now been modified

--- TestJenkins.java
+++ TestJenkins.java
@@ -1,8 +1,9 @@
 public class TestJenkins{
 		public static void main(String arg[]){
 			for (int i = 1; i<=10; i++){
-				System.out.println(i+" This is my jave for jenkin");
+				System.out.println(i+" This is my java for jenkin");
 			}
+			System.out.println("The first change has now been made to this code");
 		}
 
 
